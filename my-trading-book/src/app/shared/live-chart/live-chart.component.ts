c7fd2759e8f1b7727b831e3a17386f989aa94823
dechevgfx <101424579+dechevgfx@users.noreply.github.com>
2023-08-09 16:32:29 +0300

    style changes

--- my-trading-book/src/app/shared/live-chart/live-chart.component.ts
+++ my-trading-book/src/app/shared/live-chart/live-chart.component.ts
@@ -27,7 +27,7 @@ export class LiveChartComponent implements OnInit {
       symbol: 'NASDAQ:AAPL',
       interval: 'D',
       timezone: 'Etc/UTC',
-      theme: 'light',
+      theme: 'dark',
       style: '1',
       locale: 'en',
       toolbar_bg: '#f1f3f6',
