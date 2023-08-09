33229f9c53135327f0cbfbac6605ce7261869f5c
dechevgfx <101424579+dechevgfx@users.noreply.github.com>
2023-08-09 21:32:44 +0300

    Update environment.prod.ts

--- my-trading-book/src/environments/environment.prod.ts
+++ my-trading-book/src/environments/environment.prod.ts
@@ -1,12 +1,14 @@
 export const environment = {
   firebase: {
-    projectId: 'YOUR-FIREBASE-CONFIG',
-    appId: 'YOUR-FIREBASE-CONFIG',
-    storageBucket: 'YOUR-FIREBASE-CONFIG',
-    apiKey: 'YOUR-FIREBASE-CONFIG',
-    authDomain: 'YOUR-FIREBASE-CONFIG',
-    messagingSenderId: 'YOUR-FIREBASE-CONFIG',
+    apiKey: "AIzaSyC5FlJNu509_WEn-DnHPyNvjF4d36Fb75U",
+    authDomain: "angular-app-5cd2d.firebaseapp.com",
+    databaseURL: "https://angular-app-5cd2d-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "angular-app-5cd2d",
+    storageBucket: "angular-app-5cd2d.appspot.com",
+    messagingSenderId: "134931515764",
+    appId: "1:134931515764:web:a1d922b2ca3b1516358421",
+    measurementId: "G-715FMNPGP4"
   },
-  production: true,
+  production: false,
   apiUrl: 'http://localhost:4000'
 };
